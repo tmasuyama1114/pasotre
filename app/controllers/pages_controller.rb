5f6abfe64a4589615311a52e796db74866e2f449
tmasuyama1114 <tmasuyama1114@gmail.com>
2021-08-08 11:51:02 +0900

    complete top contents and add the base of about page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def top
   end
+
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: 'pages#top'
 
+  get 'about', to: 'pages#about'
+  
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
