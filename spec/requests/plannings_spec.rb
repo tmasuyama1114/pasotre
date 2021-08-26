21518211b774457e586746635129b1167c66a989
tmasuyama1114 <tmasuyama1114@gmail.com>
2021-08-26 22:50:40 +0900

    complete plannings and pages rspec tests

--- spec/requests/plannings_spec.rb
+++ spec/requests/plannings_spec.rb
@@ -13,6 +13,16 @@ RSpec.describe "Plannings", type: :request do
         expect(response).to have_http_status(200)
       end
 
+      it "should success without http 404" do
+        get plannings_path
+        expect(response).not_to have_http_status(404)
+      end
+
+      it "should success without http 500" do
+        get plannings_path
+        expect(response).not_to have_http_status(500)
+      end
+
       it "should success with string_Today" do
         get plannings_path
         expect(response.body).to include "Today"
@@ -29,9 +39,24 @@ RSpec.describe "Plannings", type: :request do
         expect(response).to have_http_status(200)
       end
 
+      it "should success without http 404" do
+        get new_planning_path
+        expect(response).not_to have_http_status(404)
+      end
+
+      it "should success without http 500" do
+        get new_planning_path
+        expect(response).not_to have_http_status(500)
+      end
+
       it "should success with string_make" do
         get new_planning_path
         expect(response.body).to include "make"
+      end
+
+      it "should success without string_abcdefg" do
+        get new_planning_path
+        expect(response.body).not_to include "abcdefg"
       end
   end
 
